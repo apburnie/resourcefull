25dcf96fe8e8a9a584798411454b88bbc236a492
Katie Smith <klssmith@users.noreply.github.com>
2016-02-02 15:38:10 +0000

    Adds unit test for item name validation

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :development, :test do
   gem 'byebug'
   gem 'rspec-rails'
   gem 'capybara'
+  gem 'shoulda'
+  gem 'rspec-collection_matchers'
 end
 
 group :development do
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  it 'is not valid if the name is less than 3 characters' do
+    item = Item.new(name: 'ok')
+    expect(item).to have(1).error_on(:name)
+    expect(item).not_to be_valid
+  end
+ end
