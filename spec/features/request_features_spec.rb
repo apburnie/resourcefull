2863b1d75181a3b8c055669f6a321435ab405d24
Andrew Burnie <apburnie@hotmail.co.uk>
2016-02-03 11:20:52 +0000

    enables request of specific book

--- spec/features/request_features_spec.rb
+++ spec/features/request_features_spec.rb
@@ -14,7 +14,14 @@ feature 'Request items' do
         sign_up(email:'me@email.com', name:"Katie", password:"Secret01")
         expect(current_path).to eq '/'
         expect(page).to have_content 'Ruby book'
-        expect(page).to have_link 'Request'
+        expect(page).to have_link 'Request Ruby book'
+      end
+
+      xit 'shows Katie made a request' do
+        visit '/items'
+        sign_up(email:'me@email.com', name:"Katie", password:"Secret01")
+        click_link 'Request'
+        expect(page).to have_content 'KRuby book'
       end
     end
   end
