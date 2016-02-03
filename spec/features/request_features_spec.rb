3f51e4d4cdd877266b33a9fa33c3b7d1ed6137a9
Katie Smith <klssmith@users.noreply.github.com>
2016-02-03 16:54:34 +0000

    styling forms

--- spec/features/request_features_spec.rb
+++ spec/features/request_features_spec.rb
@@ -14,14 +14,14 @@ feature 'Request items' do
         sign_up(email:'me@email.com', name:"Katie", password:"Secret01")
         expect(current_path).to eq '/'
         expect(page).to have_content 'Ruby book'
-        expect(page).to have_link 'Request Ruby book'
+        expect(page).to have_link "Request 'Ruby book'"
       end
 
       it 'shows Katie made a request' do
         visit '/items'
         sign_up(email:'me@email.com', name:"Katie", password:"Secret01")
-        click_link 'Request Ruby book'
-        expect(page).to have_content 'Katie requested Ruby book'
+        click_link "Request 'Ruby book'"
+        expect(page).to have_content "Katie requested 'Ruby book'"
         expect(page).not_to have_link 'Request Ruby book'
       end
     end
